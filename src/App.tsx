6760bfb57ba4917a91744b1091cd1fe532f813d3
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-01-16 12:41:18 -0300

    feat: add component rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import HeaderNav from './components/Header';
-import LoginForm from './components/LoginForm';
+import Routes from './Routes';
 
 function App() {
   return (
     <>
       <HeaderNav />
-      <LoginForm />
+      <Routes />
     </>
   );
 }
