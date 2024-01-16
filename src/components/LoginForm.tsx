2548eb7b456f41f6816e869a2fdc0e807d88c561
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-01-16 12:43:05 -0300

    docs: LoginForm altera formulario

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import '../ApiLaravel';
 
 const LoginForm: React.FC = () => {
     return (
         <section className='flex justify-center items-center h-[calc(100vh-4rem)]'>
-            <form className='flex flex-col justify-center items-center w-96 bg-[var(--blue)] h-3/5'>
+            <form id="login" className='flex flex-col justify-center items-center w-96 bg-[var(--blue)] h-3/4'>
                 <img src="img/iconBall.svg" alt=""  width={70} height={70}/>
                 <div>
                     <label htmlFor="loginUser">Email</label>
-                    <input type="text" name="loginUser" id="loginUser" />
+                    <input type="text" name="loginUser" id="loginUser" required/>
                 </div>
                 <div>
-                    <label htmlFor="loginUser">Senha</label>
-                    <input type="password" name="password" id="password" />
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" name="password" id="password" required/>
                 </div>
                 <div>
                     <button type='submit'>Logar</button>
+                </div>
+                <div>
+                    <a href="/register">Cadastre-se</a>
                 </div>
             </form>
         </section>
