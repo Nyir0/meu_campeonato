44cdf1a97fb6c6518f759511f2426bc1e1b3783a
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-01-16 12:42:11 -0300

    refactor: header modificado por rota

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
+// Componente do Header geral da aplicação
+
 const HeaderNav: React.FC = () => {
 
     const pathname = window.location.pathname;
 
-    if(pathname === "/"){
+    if(pathname === "/" || pathname === "/login" || pathname === "/register"){
         return(
             <header className='pl-16'>
                 <strong className='text-md font-bold'>Meu Campeonato</strong>
