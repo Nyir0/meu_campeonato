d0489a2df655c710013411554393a4013e484031
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-01-17 15:02:47 -0300

    feat: Adiciona rota para o componente de inicio

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from './components/LoginForm';
+import Home from "./components/Home";
 
 const Login: React.FC = () => {
     return(
@@ -10,6 +11,7 @@ const Login: React.FC = () => {
                 <Route path='/' element={<LoginForm />}/>
                 <Route path='/login' element={<LoginForm />}/>
                 <Route path='/register' element={<RegisterForm />}/>
+                <Route path='/Inicio' element={<Home />}/>
             </Routes>
         </Router>
     );
