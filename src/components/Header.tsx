00e6a2e32028f8798bd752f59321deab29c7760d
Gabriel Teofilo <gabriel.ol.teo@outlook.com>
2024-01-17 21:42:44 -0300

    refactor: Header esta com menos conteudo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import UrlLaravel from '../UrlLaravel';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // Componente do Header geral da aplicação
 
@@ -17,50 +15,10 @@ const HeaderNav: React.FC = () => {
         );
     }
 
-    const logout = () =>{
-        axios.get(UrlLaravel() + '/sanctum/csrf-cookie')
-            .then(() => {
-            const csrfToken = document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*=\s*([^;]*).*$)|^.*$/, "$1");
-            // Realizar a solicitação de registro
-            axios.post(UrlLaravel() + "/logout", null,{
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-XSRF-TOKEN': decodeURIComponent(csrfToken)
-                // Adicione outros cabeçalhos conforme necessário
-                },
-                withCredentials: true,
-            })
-            .then((response) => {
-                    if(response.data === "sucess") {
-                      window.location.href="/login";
-                    }
-                })
-                .catch((error) => {
-                // Trate os erros da solicitação de registro
-                console.error('Erro na solicitação de registro:', error);
-                });
-            })
-            .catch((error) => {
-            // Trate os erros na obtenção do token CSRF
-            console.error('Erro ao obter token CSRF:', error);
-        });
-    }
-
     return(
         <header>
             <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to="/perfil">Perfil</Link>
-                    </li>
-                </ul>
-            </nav>
             </Router>
-            <span id="logout" className='w-20 cursor-pointer' onClick={logout}>Sair</span>
         </header>
     );
 }
